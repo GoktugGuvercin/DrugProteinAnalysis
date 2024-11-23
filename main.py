036b4d43bf4ed3c88ed37c5c1940737cc6ff5ae1
goktug <goktug150140@gmail.com>
2024-11-24 00:19:20 +0100

    changing dict-db to dataframe-db and adding uniprot query function

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from utils import ProteinDB
 
 fasta_dir = "./human_proteome_reviewed.fasta"
-protein_db = ProteinDB(fasta_dir)
+protein_db = ProteinDB("FASTA", "", fasta_dir)
+protein_db.query_to_uniprot("A1L4Q6")
 
 # search by uniprot id
-seq, gene, exp = protein_db.search_uniprot_id("Q7LG56")
-print("Protein Q7LG56: ")
-print("Sequence: ", seq)
-print("Gene name: ", gene)
-print("Description: ", exp)
+uniprot_id = "Q7LG56"
+entry = protein_db.search_uniprot_id(uniprot_id)
+print("\nUniprot Id: ", uniprot_id)
+print(entry[:-1])
+print(entry[-1])
 print()
 
 # search by gene name
 proteins = protein_db.search_gene("AKAP7")
-for protein in proteins:
-    print("Uni-prot id: ", protein[0])
-    print("Sequence: ", protein[1])
-    print("Description: ", protein[2])
-    print()
+print(proteins)
+print()
+
+uniprot_id = "Q95330"  # rabbit tp53
+entry = protein_db.query_to_uniprot(uniprot_id)
+print(entry)
+print()
+
+entry = protein_db.search_uniprot_id(uniprot_id)
+print(entry)
+print()
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
+import requests as r
+import pandas as pd
 from Bio import SeqIO
+from io import StringIO
+
+base_url = "http://www.uniprot.org/uniprot/"
 
 
 def is_string(name: str) -> bool:
@@ -12,125 +17,174 @@ def is_string(name: str) -> bool:
     return False
 
 
-def parse_fasta_header(title: str) -> tuple:
-    """ Derive protein description and gene name from fasta title.
+def parse_fasta_header(title: str) -> list:
+    """ Derive entries (protein name, gene, species etc.) from fasta header.
 
-    * Fasta titles contain uniprot entry, protein description, organism
+    * Fasta headers contain uniprot entry, protein name, organism
     species (OS), organism taxonomoy (OX), protein existence (PE), and
-    sequence version (SV). This function parses them and fetch protein
-    explanation with respective gene name.
+    sequence version (SV). This function at first parses them and then
+    return in a list.
 
     Args:
       title: title of a fasta file in string format
 
-    Returns: a tuple of protein description and gene name
+    Returns: a list of fasta-header entries
     """
-    content = " ".join(title.split(" ")[1:])
-    os_index = content.find("OS=")  # complexity = O(3N)
-    gn_index = content.find("GN=")  # complexity = O(3N)
 
-    gene = content[gn_index + 3:].split(" ")[0]
-    gene = "##" if len(gene) == 1 else gene
-    description = content[: (os_index - 1)]
+    prot_name = title[:title.find("_")].split("|")[-1]
+    entries = {"Protein": prot_name, "OS": "##", "OX": "##",
+               "GN": "##", "PE": "##", "SV": "##"}
+    parts = title.split()
 
-    return description, gene
+    for part in parts:
+        if "=" in part:
+            key, value = part.split("=", 1)
+            entries[key] = value
+
+    return list(entries.values())
 
 
 def read_fasta(input_file: str) -> tuple:
 
-    """ Returns all prot-ids, sequences, genes, prot-descriptions in fasta.
+    """ Returns prot-ids, sequences, and headers retrieved from fasta.
 
     * A fasta file can contain more than one protein entry. Each entry
-    contains specific fasta header, and protein sequence. This function
-    reads all the context and decomposes it into uniprot ids, sequences,
-    genes, and descriptions.
+    contains specific fasta header, uniprot id, protein sequence. This
+    function reads all the context and decomposes it into uniprot ids,
+    sequences, and headers in list format.
 
     Args:
       input_file: the directory of fasta file in str format
 
-    Returns: A tuple of uniprot ids, sequences, genes and descriptions in
+    Returns: A tuple of uniprot ids, sequences, and headers in
       list format
     """
 
-    uniprot_ids, fasta_seqs = [], []
-    explanations, genes = [], []
+    uniprot_ids, seqs, headers = [], [], []
     fasta_sequences = SeqIO.parse(open(input_file), 'fasta')
 
     for fasta in fasta_sequences:
         uniprot_ids.append(fasta.id.split("|")[1])
-        fasta_seqs.append(str(fasta.seq))
+        seqs.append(str(fasta.seq))
+        header = parse_fasta_header(fasta.description)
+        headers.append(header)
 
-        exp, gene = parse_fasta_header(fasta.description)
-        explanations.append(exp)
-        genes.append(gene)
-
-    return uniprot_ids, fasta_seqs, genes, explanations,
+    return uniprot_ids, seqs, headers
 
 
 class ProteinDB:
 
     def __init__(
             self,
-            huproteome_reviewed_dir: str,
+            db_choice: str,
+            database_csv_dir: str,
+            proteome_reviewed_dir: str,
     ) -> None:
 
         """ Defines protein database.
 
         Args:
+          db_choice: which dir is chosen for db construction (CSV or FASTA)
+          database_csv_dir: the directory of csv file containing protein entries
           huproteome_reviewed_dir: the directory of human proteins fasta file [1]
 
         [1]: https://www.uniprot.org/proteomes/UP000005640
         """
 
-        contents = read_fasta(huproteome_reviewed_dir)
-        prot_ids, seqs, genes, explanations = contents
+        if db_choice.upper() == "FASTA":
+            contents = read_fasta(proteome_reviewed_dir)
+            prot_ids, seqs, headers = contents
+            prots, os, ox, gn, pe, sv = zip(*headers)
 
-        self.pid_db = {}
-        self.gene_db: dict[str, list] = {gene: [] for gene in genes}
+            pid_db = {"ID": prot_ids, "Protein Name": prots,
+                      "Gene": gn, "Species": os, "Taxonomy": ox,
+                      "PE": pe, "SV": sv, "Sequence": seqs}
 
-        for pid, seq, gene, exp in zip(*contents):
-            self.pid_db[pid] = (seq, gene, exp)
-            self.gene_db[gene].append((pid, seq, exp))
+            self.database = pd.DataFrame(pid_db).set_index("ID")
 
-    def search_uniprot_id(self, uniprot_id: str) -> tuple:
+        else:
+            self.database = pd.read_csv(database_csv_dir)
 
+    def search_uniprot_id(self, uniprot_id: str) -> list:
         """It returns sequence, gene, and description of target protein.
 
-        * The function search for uniprot id given as input argument in
-        human proteome dictionary. If it exists, it returns respective
-        sequence, gene name, and description context.
+        * The function searches for uniprot id given as input argument in
+        the DataFrame. If it exists, the features of target protein is returned
+        [protein name, gene, species, taxonomy, PE, SV, sequence].
 
         Args:
-          uniprot_id: human uniprot id in string format.
+          uniprot_id: uniprot id in string format.
 
-        Returns: a tuple of sequence, gene, protein description.
+        Returns: a list of corresponding protein features in str.
         """
 
         is_string(uniprot_id)
         uniprot_id = uniprot_id.upper()
 
-        if uniprot_id not in self.pid_db:
-            raise ValueError("Invalid uniprot id for reviewed human proteome")
-        return self.pid_db[uniprot_id]
+        if uniprot_id not in self.database.index:
+            raise ValueError("Invalid uniprot id for proteome database")
+        return self.database.loc[uniprot_id].tolist()
 
-    def search_gene(self, gene: str) -> list[tuple]:
-
+    def search_gene(self, gene: str) -> pd.DataFrame:
         """It returns uniprot id, sequence, and description of target protein.
 
-        * The function search for gene name given as input argument in
-        human proteome dictionary. If it exists, it returns respective
-        uniprot ids, sequences, and description context. One gene name can
-        refer to multiple uniprot ids, because multiple protein isoforms can
-        be synthesized from one gene by alternative splicing such as AKAP7.
+        * The function searches for gene name given as input argument in
+        the DataFrame. One gene can refer to more then one uniprot id, because
+        multiple protein isoforms can be synthesized from one gene by
+        alternative splicing such as AKAP7. Hence, all proteins matched with
+        given gene name are returned as a DataFrame object.
 
         Args:
           gene: gene name in string format.
 
-        Returns: a tuple of three lists (sequences, genes, protein descriptions).
+        Returns: a DataFrame object that can contain multiple protein entries.
         """
 
         is_string(gene)
         gene = gene.upper()
-        if gene not in self.gene_db:
-            raise ValueError("Invalid gene name for reviewed human proteome")
-        return self.gene_db[gene]
+        if gene not in self.database["Gene"].to_list():
+            raise ValueError("Invalid gene name for proteome database")
+
+        matches = (self.database["Gene"] == gene)
+        return self.database[matches]
+
+    def query_to_uniprot(self, uniprot_id: str) -> pd.DataFrame:
+        """ Fetches a protein entry from uniprot and save it to database.
+
+        * The function sends an API request to UniProt to search for
+        particular uniprot id. If it exists, the response of the request
+        is converted to fasta and parsed into a new protein entry, which
+        is saved into protein database.
+
+        Args:
+          uniprot_id: uniprot id in string format.
+        """
+
+        # sending query to UniProt
+        fasta_url = f"{base_url}{uniprot_id}.fasta"
+        response = r.post(fasta_url)
+
+        if response.status_code == 404:
+            raise ValueError("Invalid uniprot id is passed as input argument")
+
+        # converting query response to fasta
+        fasta_str = StringIO(response.text)
+        fasta = list(SeqIO.parse(fasta_str, 'fasta'))[0]
+
+        # parsing fasta content
+        seq = str(fasta.seq)
+        uniprot_id = fasta.id.split("|")[1]
+        header = parse_fasta_header(fasta.description)
+        prot, os, ox, gn, pe, sv = header
+
+        # adding new entry
+        entry = {"Protein Name": prot, "Gene": gn, "Species": os,
+                 "Taxonomy": ox, "PE": pe, "SV": sv, "Sequence": seq}
+        df_entry = pd.DataFrame(entry, index=[uniprot_id])
+        self.database = pd.concat([self.database, df_entry])
+
+        return df_entry
+
+    def save_to_csv(self, save_dir: str) -> None:
+        """Saves protein database into csv file."""
+        self.database.to_csv(save_dir)
