ffe690203cdc8391005762d6d4cd31969d09c27c
goktug <goktug150140@gmail.com>
2024-11-24 13:51:37 +0100

    pyrrolysine test case added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import ProteinDB
+from utils import ProteinDB, has_pyrrolysine
 
 fasta_dir = "./human_proteome_reviewed.fasta"
 protein_db = ProteinDB("FASTA", "", fasta_dir)
@@ -17,11 +17,19 @@ proteins = protein_db.search_gene("AKAP7")
 print(proteins)
 print()
 
+# send query to uniprot api
 uniprot_id = "Q95330"  # rabbit tp53
 entry = protein_db.query_to_uniprot(uniprot_id)
 print(entry)
 print()
 
+# search for rabbit tp53 in our database
 entry = protein_db.search_uniprot_id(uniprot_id)
 print(entry)
 print()
+
+# send query to uniprot api
+uniprot_id = "O30642"  # Methanosarcina barkeri, MtmB1
+entry = protein_db.query_to_uniprot(uniprot_id)
+seq = entry["Sequence"].values[0]
+print(has_pyrrolysine(seq))
