e70d6f0bdc210e1fa3a81b97ce271db38d6a2ebe
goktug <goktug150140@gmail.com>
2025-02-14 01:40:19 +0100

    repository name change and update in source file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Cancer Research
+# DrugProteinAnalysis
 
-This repository is dedicated to cancer research. It will higlight theoretical knowledge-base together with related deep learning projects. 
+This repository is dedicated to drug and protein analysis. It will higlight theoretical knowledge-base together with related deep learning projects. 
 
 1. [Cancer Drugs](https://github.com/GoktugGuvercin/Cancer-Research/blob/main/Drugs.md)
 2. [Protein Database](https://github.com/GoktugGuvercin/Cancer-Research/blob/main/utils.py)
--- scripts/pfam_main.py
+++ scripts/pfam_main.py
@@ -1,13 +1,17 @@
 from argparse import Namespace
 import numpy as np
-from scripts.pfam import Pfam
-from scripts.utils import load_embeds_pickle
+from pfam import Pfam
+from utils import load_embeds_pickle
 
 pfams_dir = "/Users/goktug/Desktop/Cancer-Research/data/genes_pfams.tsv"
 embeds_dir = "/Users/goktug/Desktop/Cancer-Research/data"
 
 protbert_embeds = load_embeds_pickle(embeds_dir, "prot_bert_embeds.p")
 pfamily = Pfam(pfams_dir, "Gene")
+pfam_groups = pfamily.detect_large_pfams(120)
+
+print("Large Pfam Groups:")
+print(pfam_groups)
 
 query_genes = list(protbert_embeds.keys())
 query_embeds = np.array(list(protbert_embeds.values()))
@@ -18,6 +22,6 @@ config.perplexity = 50
 config.init = "random"
 config.learning_rate = "auto"
 
-target_pfams = ["PF00096", "PF00069"]
+target_pfams = ["PF00096;PF01352", "PF00069"]
 
 pfamily.apply_tsne(query_genes, query_embeds, config, target_pfams)
